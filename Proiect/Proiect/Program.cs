85f2599ccfcfb2a6b4fdced3400b5528b8dcb707
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-01-03 15:55:37 +0200

    add Quantity.cs class

--- Proiect/Proiect/Program.cs
+++ Proiect/Proiect/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Proiect.domain.models;
+
+namespace Proiect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Quantity.IQuantity a = new Quantity.Units(123);
+            Quantity.IQuantity b = new Quantity.Undefined("asdasd");
+
+            Console.WriteLine(a.ToString() + " " + a.GetType() + "\n" + b.ToString() + " " + b.GetType());
+        }
+    }
+}
---
+++ Proiect/Proiect/domain/models/Quantity.cs
@@ -0,0 +1,34 @@
+using CSharp.Choices;
+
+namespace Proiect.domain.models
+{
+    [AsChoice]
+    public static class Quantity
+    {
+        public interface IQuantity { };
+        public record Units : IQuantity
+        {
+            public Units(int number)
+            {
+                this.number = number;
+            }
+            public override string ToString()
+            {
+                return $"{this.number}";
+            }
+            public int number {  get; init; }
+        }
+        public record Undefined : IQuantity
+        {
+            public Undefined(string undefined)
+            {
+                this.undefined = undefined;
+            }
+            public override string ToString()
+            {
+                return $"{this.undefined}";
+            }
+            public string undefined { get; init;}
+        }
+    }
+}
