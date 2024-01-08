56650a4d75e29be1d08eb34f29aa9f1e0d47afcf
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-01-08 20:20:28 +0200

    remove Program.cs commented code

--- Proiect/Proiect/Program.cs
+++ Proiect/Proiect/Program.cs
@@ -8,40 +8,6 @@ namespace Proiect
     {
         static void Main(string[] args)
         {
-            /*List<UnvalidatedProduct> products = AvailableProducts.LoadProducts();
-            List<Product> checkProducts;
-            Contact contact = new Contact("asdas", "belfast", "1234567890", "asaada");
-            IShoppingCart shoppingCart = new EmptyShoppingCart(contact);
-            products.ForEach(product => { shoppingCart = AddProductToShoppingCart(shoppingCart, product); });
-            checkProducts = GetShoppingCartItems(shoppingCart);
-            checkProducts.ForEach(product => { Console.WriteLine(product.ToString()); });
-            UnvalidatedProduct newProduct = new UnvalidatedProduct("asdasd", "11", "22");
-            shoppingCart = AddProductToShoppingCart(shoppingCart, newProduct);
-            checkProducts = GetShoppingCartItems(shoppingCart);
-            checkProducts.ForEach(product => { Console.WriteLine(product.ToString()); });
-            shoppingCart = RemoveProductFromShoppingCart(shoppingCart, "asdasd");
-            checkProducts = GetShoppingCartItems(shoppingCart);
-            checkProducts.ForEach(product => { Console.WriteLine(product.ToString()); });
-            shoppingCart = ValidateShoppingCart((UnvalidatedShoppingCart)shoppingCart);
-            shoppingCart = CalculateShoppingCart(shoppingCart);
-
-            switch (shoppingCart)
-            {
-                case CalculatedShoppingCart calculatedCart:
-                    Console.WriteLine("Final Validated and Calculated Shopping Cart:");
-                    calculatedCart.ValidatedProducts.ForEach(p => { Console.WriteLine(p.ToString()); });
-                    Console.WriteLine($"Total Price: {calculatedCart.FinalPrice}");
-                    break;
-                case InvalidShoppingCart invalidCart:
-                    Console.WriteLine("Invalid Shopping Cart:");
-                    invalidCart.InvalidProducts.ForEach(p => { Console.WriteLine(p.ToString()); });
-                    Console.WriteLine($"Reason for invalidation: {invalidCart.Reason}");
-                    break;
-                default:
-                    Console.WriteLine("Unknown or unchanged shopping cart state");
-                    break;
-            }*/
-
             //init
             Contact contact = new Contact("asd", "asd", "1111111111", "asd");
             IShoppingCart shoppingCart = new EmptyShoppingCart(contact);
